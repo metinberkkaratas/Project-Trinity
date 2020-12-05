0b0eb79852629c639d810955dac727d8fb350bef
Metin Berk Karataş <43265537+metinberkkaratas@users.noreply.github.com>
2020-12-05 20:49:43 +0300

    Added basic basic terrain, demo chracter, camera movement and isometric camera. Used asset editor for chracter. Basic terrain not scaled well. (#1)

---
+++ Assets/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public float scrollSpeed = 2000f;
+    
+    
+    public Vector2 panLimit;
+    public float minY = 20f;
+    public float maxY = 120f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        
+        if (Input.GetKey(KeyCode.UpArrow) || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+        
+        if (Input.GetKey(KeyCode.RightArrow) || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+            pos.x -= panSpeed * Time.deltaTime;
+        } 
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * scrollSpeed * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+
+        transform.position = pos;
+
+    }
+}
